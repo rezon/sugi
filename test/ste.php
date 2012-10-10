0dde918efae51fd17246e5d712ef5d84a34ad36c
Plamen Popov <tzappa@bulinfo.net>
2012-10-10 10:39:32 +0300

    cleanup

--- test/ste.php
+++ test/ste.php
@@ -12,9 +12,6 @@ $tpl = new Ste();
 $tpl->load('ste/index.html');
 $tpl->set('title', 'STE');
 $tpl->set(array('description' => 'Simple Template Engine', 'keywords' => 'php, template, engine, sugi'));
-<<<<<<< HEAD
-$tpl->set('home', array('link' => array('href' => '#', 'title' => 'top')));
-=======
 $tpl->set('home', array('link' => array('href' => 'index.php', 'title' => 'back')));
->>>>>>> 5675b147ba5bd59feda1700dca97f672b3c0d36c
+
 echo $tpl->parse();
