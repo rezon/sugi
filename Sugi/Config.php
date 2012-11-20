1e3a879e491f3b6db827b2aebd2e01bd24532ad7
Plamen Popov <tzappa@gmail.com>
2012-11-21 00:05:34 +0200

    reformat docblock

--- Sugi/Config.php
+++ Sugi/Config.php
@@ -8,35 +8,41 @@ include_once __DIR__."/File.php";
 
 /**
  * Simplifies configurations via automatic loading and caching configuration files.
- * Unlike Registry Config if trying to load configuration files on demand.
+ * Unlike Registry, Config is trying to load configuration files on demand.
  *
  * To get a values from database config file use:
  * <code>
- * 		// simple (no file auto-loading) config
- * 		Config::set('debug', TRUE);
- * 		Config::get('debug'); 							// returns TRUE;
- * 		Config::set('test', array('foo' => 'bar'));
- * 		Config::get('test'); 							// returns an array
- * 		Config::get('test.foo'); 						// returns 'bar'
- * 		Config::get('test.bar'); 						// returns NULL
- * 		Config::get('test.bar', 'foobar'); 				// returns 'foobar'
+ * // simple (no file auto-loading) config
+ * Config::set('debug', TRUE);
+ * Config::get('debug'); // returns TRUE;
+ * 
+ * Config::set('test', array('foo' => 'bar'));
+ * Config::get('test'); // returns an array
+ * Config::get('test.foo'); // returns 'bar'
+ * Config::get('test.bar'); // returns NULL
+ * Config::get('test.bar', 'foobar'); // returns 'foobar'
  *
- * 		// before using Config with auto-loading functions it is good to set special config item, which will resolve the search path
- * 		Config::set('_path', '/your/full/path/to/config');
+ * // before using Config with auto-loading functions it is good to set 
+ * // special config item, which will resolve the search path
+ * Config::set('_path', '/your/full/path/to/config');
  * 		
- * 		$dbconf = Config::database(); 					// returns an array if the file 'database.conf.php' exists in the search path
- * 		$dbname = Config::database('name');				// returns an item from the config file 'database.conf.php'
- * 		$host = Config::database('host', 'localhost'); 	// returns an item, and if the item does not exists will return your default value - 'localhost'
- * 		$tz = Config::application('default.timezone'); 	// returns an item timezone from the array returned from the config file 'application.conf.php'
+ * $dbconf = Config::database(); 
+ * 	// returns an array if the file 'database.conf.php' exists in the search path
+ * $dbname = Config::database('name'); 
+ * 	// returns an item from the config file 'database.conf.php'
+ * $host = Config::database('host', 'localhost'); 
+ * 	// returns an item, and if the item does not exists will return your default value - 'localhost'
+ * $tz = Config::application('default.timezone'); 
+ * 	// returns an item timezone from the array returned from the config file 'application.conf.php'
  *   	
- *   	// return value when trying to get an item from some file that does not exist will be null, or your default value
- *   	Config::unexistingconfig(); 					// returns NULL
- *   	Config::unexistingconfig('foo'); 				// returns NULL
- *   	Config::unexistingconfig('foo', 'bar'); 		// returns 'bar'
+ * // return value when trying to get an item from some file that does 
+ * // not exist will be null or your default value
+ * Config::unexistingconfig(); // returns NULL
+ * Config::unexistingconfig('foo'); // returns NULL
+ * Config::unexistingconfig('foo', 'bar'); // returns 'bar'
+ *</code>
  *
- * </code>
- *
- * The name of the config file can be anything, but 'set' and 'get' - which are used as a simple registry (without file auto-loading).
+ * The name of the config file can be anything, but 'set', 'get' - which are used as a simple registry (without file auto-loading).
  * 
  * The values can be invoked with dot notation like Config::get('key.subkey').
  * 
@@ -46,14 +52,20 @@ include_once __DIR__."/File.php";
  */
 class Config
 {
-	// cache of configuration items
+	/**
+	 * Cache of configuration items
+	 */
 	protected static $registry = array();
 
-	// cache of loaded files
+	/**
+	 * Names of all loaded files which prevents loading them more then once 
+	 */
 	protected static $files_registry = array();
 
-	// accepted extensions and search file order
-	protected static $ext_order = array('.conf.php', '.json');
+	/**
+	 * Accepted extensions and search file order
+	 */
+	public static $ext_order = array('.conf.php', '.json');
 
 	/**
 	 * Magic method, which is responsible for auto-loading configuration files
@@ -112,11 +124,25 @@ class Config
 		return static::_extract(static::$registry, $key, $default);
 	}
 
-	// the following functions are reserved for further use
-	public static function path() { throw new \Exception("This function is reserved for further use"); }
-	public static function register() { throw new \Exception("This function is reserved for further use"); }
-	public static function unregister() { throw new \Exception("This function is reserved for further use"); }
-	public static function isRegistered() { throw new \Exception("This function is reserved for further use"); }
+	/**
+	 * Reserved for further use
+	 */
+	protected static function path() { }
+
+	/**
+	 * Reserved for further use
+	 */
+	protected static function register() { }
+	
+	/**
+	 * Reserved for further use
+	 */
+	protected static function unregister() { }
+
+	/**
+	 * Reserved for further use
+	 */
+	protected static function isRegistered() { }
 
 	/**
 	 * Search and load configuration file
