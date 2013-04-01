3fc6a174420474a5ed85f8493513f02bf645c71b
Plamen Popov <tzappa@gmail.com>
2013-04-01 16:06:00 +0300

    Event uses SugiPHP\Events

--- Sugi/Event.php
+++ Sugi/Event.php
@@ -1,51 +1,51 @@
-<?php namespace Sugi;
+<?php
 /**
  * @package Sugi
  * @author  Plamen Popov <tzappa@gmail.com>
  * @license http://opensource.org/licenses/mit-license.php (MIT License)
  */
 
-use \Sugi\Event\Dispatcher;
+namespace Sugi;
+
+use \SugiPHP\Events\Event as Evnt;
+use \SugiPHP\Events\Dispatcher;
 
 /**
- * Facades Sugi\Event\Dispatcher and Sugi\Event\Event for lazy people like me
+ * Facades SugiPHP\Events\Dispatcher and SugiPHP\Events\Event for lazy people like me
  *
  * There are 2 basic static methods:
  *  - listen - tells the dispatcher to trigger given callback function when some event occurs
  *  - fire   - creates an event and through the dispatcher notifies all listeners for that event
  */
-class Event
+class Event extends Facade
 {
-	protected static $dispatcher = null;
+	protected static $instance = null;
+
+	/**
+	 * @inheritdoc
+	 */
+	protected static function _getInstance()
+	{
+		if (!static::$instance) {
+			static::$instance = new Dispatcher();
+		}
+
+		return static::$instance;
+	}
 
 	public static function listen($eventName, $callback)
 	{
-		if (is_null(static::$dispatcher)) {
-			static::$dispatcher = new Dispatcher();
-		}
-		
-		static::$dispatcher->listen($eventName, $callback);
+		$dispatcher = static::_getInstance();
+		$dispatcher->addListener($eventName, $callback);
 	}
 
 	public static function fire($eventName, array $params = array())
 	{
-		if (is_null(static::$dispatcher)) {
-			static::$dispatcher = new Dispatcher();
-		}
+		$dispatcher = static::_getInstance();
 
-		$event = new Event\Event($eventName, $params);
-		$event->setDispatcher(static::$dispatcher);
+		$event = new Evnt($eventName, $params);
+		$event->setDispatcher($dispatcher);
 
-		static::$dispatcher->fire($event);
-	}
-
-	public static function hasListeners($eventName)
-	{
-		if (is_null(static::$dispatcher)) {
-			return false;
-		}
-
-		$listeners = static::$dispatcher->getListeners($eventName);
-		return !empty($listeners);
+		$dispatcher->dispatch($event);
 	}
 }
